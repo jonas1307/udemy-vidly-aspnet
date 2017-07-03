53a1eeda5cbc4c80d5d675f9ce13b0c44a3dfdbc
Jonas Lima <jonas1307@live.com>
2017-07-02 23:21:06 -0300

    Data Annotations for Movie

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -12,15 +12,18 @@ namespace Vidly.Models
         public Genre Genre { get; set; }
 
         [Required]
+        [Display(Name = "Genre")]
         public int GenreId { get; set; }
 
         [Required]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
         [Required]
         public DateTime DateAdded { get; set; }
 
         [Required]
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
     }
 }
