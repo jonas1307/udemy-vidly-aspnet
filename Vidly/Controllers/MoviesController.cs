07cbc46c455337e83fee31a1cc2e19a0bc83cd93
Jonas Lima <jonas1307@live.com>
2017-07-02 23:11:11 -0300

    Exercise: Add New/Edit to Movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -48,7 +48,13 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            return View(movie);
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
         }
 
         // GET /Movies
@@ -61,11 +67,45 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
         //Novo routing
         [Route("movies/released/{year}/{month}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.First(f => f.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
--- Vidly/ViewModels/CostumerFormViewModel.cs
+++ Vidly/ViewModels/CostumerFormViewModel.cs
@@ -5,6 +5,15 @@ namespace Vidly.ViewModels
 {
     public class CostumerFormViewModel
     {
+        public CostumerFormViewModel()
+        {
+            Costumer = new Costumer { Id = 0 };
+        }
+        public string Title
+        {
+            get { return (Costumer.Id == 0 ? "New" : "Edit") + " Costumer"; }
+        }
+
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
 
         public Costumer Costumer { get; set; }
