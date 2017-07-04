6e9ee9a819ea7e82e5e0cef9833200bc21252077
Jonas Lima <jonas1307@live.com>
2017-07-03 23:26:51 -0300

    Changes to Model

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
@@ -10,17 +11,38 @@ namespace Vidly.ViewModels
     {
         public MovieFormViewModel()
         {
-            Movie = new Movie { Id = 0 };
+            Id = 0;
         }
 
-        public Movie Movie { get; set; }
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
+        }
+
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public int? GenreId { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Number in Stock")]
+        public int? NumberInStock { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
 
-        public string Title
-        {
-            get { return (Movie.Id == 0 ? "New" : "Edit") + " Movie"; }
-        }
-
+        public string Title => (Id == 0 ? "New" : "Edit") + " Movie";
     }
 }
