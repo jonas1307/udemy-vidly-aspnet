00032974c29e06c9b2b8b2a2bcfd48c461a30eac
Jonas Lima <jonas1307@live.com>
2017-08-26 21:48:48 -0300

    Social Login

--- Vidly/App_Start/Startup.Auth.cs
+++ Vidly/App_Start/Startup.Auth.cs
@@ -55,7 +55,7 @@ namespace Vidly
             //   consumerSecret: "");
 
             //app.UseFacebookAuthentication(
-            //   appId: "",
+            //   appId: "853635438128880",
             //   appSecret: "");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
