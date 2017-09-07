6ea5ae30e9ae52b4265d95c48e04d3423091b461
Jonas Lima <jonas1307@live.com>
2017-09-07 16:06:11 -0300

    Remove Memory cache

--- Vidly/Controllers/CostumersController.cs
+++ Vidly/Controllers/CostumersController.cs
@@ -76,12 +76,12 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            if (MemoryCache.Default["Genres"] == null)
-            {
-                MemoryCache.Default["Genres"] = _context.Genres.ToList();
-            }
+            //if (MemoryCache.Default["Genres"] == null)
+            //{
+            //    MemoryCache.Default["Genres"] = _context.Genres.ToList();
+            //}
 
-            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
+            //var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
 
             return View();
         }
